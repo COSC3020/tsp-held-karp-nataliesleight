59be7b8006681b2d2ea6f77c4a3a0bceee9e75cc
nataliesleight <nsleight@uwyo.edu>
2025-05-02 22:20:22 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,37 +1,53 @@
 function tsp_hk(distance_matrix) {
-    var cityNum = distance_matrix.length;   // hold number of nodes
+    var n = distance_matrix.length;         // hold number of nodes， ease of use
     var shortestPath = Infinity;            // save shortest path as infinity, any found path will be lesser
-    var totalSubsets = 1 << cityNum;        // 2^n, number of paths
     
-    if (cityNum == 0 || cityNum == 1) {return 0;}     // if graph empty or one node, return empty (no paths to form)
+    if (n == 0 || n == 1) {return 0;}       // if graph empty or one node, return empty (no paths to form)
     
-    for (var start = 0; start < cityNum; start++) {   // no specific start node, check with each node as start
-   
-      var dp = Array.from({ length: totalSubsets }, () => Array(cityNum).fill(Infinity));   // create 2d array, subset x n, fill with infinity
-      dp[1 << start][start] = 0;                      // mark start node as 0, no path to start required
-
-      for (var i = 0; i < totalSubsets; i++) {        // going through array dp
-        for (var j = 0; j < cityNum; j++) {
-          if ((i & (1 << j)) === 0) {continue;}       // if current visited, continue
-
-          for (var v = 0; v < cityNum; v++) {
-            if ((i & (1 << v)) !== 0) {continue;}
-    
-            var newMask = i | (1 << v);
-            dp[newMask][v] = Math.min(dp[newMask][v], dp[i][j] + distance_matrix[j][v]);  // mark visited, update path and distance
-          }
-        }
-      }
-
-    var fullMask = (1 << cityNum) - 1;    // number of masks
-
-    for (var v = 0; v < cityNum; v++) {   // checking paths
-      if (v === i) {continue;} 
-      shortestPath = Math.min(shortestPath, dp[fullMask][v]);   // get shortest path
+    var unvisitedNodes = [];            // set to keep track of unvisited nodes
+    for (var j = 0; j < n; j++) {
+        unvisitedNodes.push(j);         // fill unvisited with all nodes (none visited yet)
     }
 
-  }
+    for (var i = 0; i < n; i++) {           // no specific start node, check with each node as start
+        
+        var memo = {};                      // key-value pairs for memoization, declare here to reset for recursion
+        var currentDist;                    // save distance to compare to shortest found distance
+        currentDist = reMemo(distance_matrix,i,unvisitedNodes,memo);       // get shortest distance from current node, calls recursive function
+        if (currentDist < shortestPath) {shortestPath = currentDist;}      // if found distance shorter, save
+    }
 
   return shortestPath;
     
+}
+
+
+
+function reMemo(graph,currentNode,unvisitedNodes,memo) { 
+    var tmpPath = Infinity;                                   // will store shortest path found with this start node
+    var currentPath = 0;                                      // stores current calculated path
+    var key = currentNode + '|' + [...unvisitedNodes].sort(); // key that starts with beginning node and saves other unvisited nodes
+    if (memo[key] != undefined) {                             // if value at that key already exists, return value
+        return memo[key];
+    }
+    if (unvisitedNodes.length == 2) {
+        return graph[unvisitedNodes[0]][unvisitedNodes[1]];     //return only path left, undirected so 0-1 or 1-0 will be same
+    }
+    else {
+        for (var k = 0; k < unvisitedNodes.length; k++) {           // looping through unvisited cities
+            if (unvisitedNodes[k] == currentNode) { continue; }     // don't count start node, is visited
+            // remove current node from unvisited nodes
+            var indexNode = unvisitedNodes.indexOf(currentNode);    // get index of current node
+            var tmpUN = [...unvisitedNodes];                        // make shallow copy of unvisited
+            if (indexNode > -1) {                                   // if value found
+                tmpUN.splice(indexNode, 1);                         // remove
+            }
+            // currentPath = lower path + current path
+            currentPath = reMemo(graph, unvisitedNodes[k], tmpUN, memo) + graph[currentNode][unvisitedNodes[k]];   
+            if (currentPath < tmpPath) {tmpPath = currentPath;}     // if found path is smaller, save
+        }
+        memo[key] = tmpPath;                                        // save path in memoization 
+    }  
+
+    return tmpPath;
 }
